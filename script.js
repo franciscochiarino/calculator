a71d5a1f87f75f0b2f2b7eea409d3f43b3c65b38
fran <francisco.chiarino@gmail.com>
2020-02-21 15:54:02 +0100

    Save before starting new solution in script.js

--- script.js
+++ script.js
@@ -11,26 +11,3 @@ const take = (a, b) => a - b;
 const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 const clear = () => display.innerHTML = '';
-
-const showCharacter = event => {
-    const character = event.target;
-    
-    if (!character.className.includes('hide')) {
-        displayOperation.innerHTML += character.innerHTML;
-    }
-    // if (character.id === 'plus') {
-    //     numbers.push(parseInt(display.innerHTML, 10));
-    //     console.log(numbers)
-    //     display.innerHTML = sum(numbers[0], numbers[1]);
-    //     numbers.shift();
-    //     console.log(numbers)
-    //     console.log(typeof display.innerHTML)
-        
-    // }
-}
-
-
-// Show what I press on display
-buttons.forEach(button => {
-    button.addEventListener('click', showCharacter);
-})
