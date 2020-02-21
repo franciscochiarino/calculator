097936c947010131426bfad3d66f35fce2cc5ba1
fran <francisco.chiarino@gmail.com>
2020-02-22 00:22:55 +0100

    Created displayContent function (forgot to commit a few steps)

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('button');
-const displayResult = document.getElementById('display-result');
-const displayOperation = document.getElementById('display-operation');
+const display = document.getElementById('display');
 const equals = document.getElementById('equals');
 const numbers = [0];
 
@@ -11,3 +10,34 @@ const take = (a, b) => a - b;
 const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 const clear = () => display.innerHTML = '';
+
+// Display 
+const displayContent = event => {
+
+    // Numbers
+    if (event.target.className === 'number') {
+        display.innerHTML += event.target.innerHTML;
+    }
+
+    // Clear
+    if (event.target.innerHTML === 'C') {
+        display.innerHTML = '';
+    }
+
+    // Operators
+    if (event.target.className === 'operator') {
+
+        // Check if there is a number
+        let displayLastCharacter = parseInt(display.innerHTML[display.innerHTML.length - 1], 10);
+        
+        if (displayLastCharacter >= 0 && displayLastCharacter <= 9) {
+            display.innerHTML += event.target.innerHTML;
+        } else {
+            console.log('vamooo')
+        }
+    }
+    
+}
+
+
+buttons.forEach(button => button.addEventListener('click', displayContent))
