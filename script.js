47c22a735f91839c6267d95d0b43be845c28ec6e
fran <francisco.chiarino@gmail.com>
2020-02-22 02:04:07 +0100

    Operators work fine

--- script.js
+++ script.js
@@ -1,10 +1,8 @@
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
 const equals = document.getElementById('equals');
-const numbers = [0];
 
 // Functions
-
 const sum = (a, b) => a + b;
 const take = (a, b) => a - b;
 const divide = (a, b) => a / b;
@@ -31,9 +29,34 @@ const displayContent = event => {
         let displayLastCharacter = parseInt(display.innerHTML[display.innerHTML.length - 1], 10);
         
         if (displayLastCharacter >= 0 && displayLastCharacter <= 9) {
-            display.innerHTML += event.target.innerHTML;
+
+            //Check if there already is an operator and run the corresponding function
+            if (display.innerHTML.includes('+')) {
+                let splitedDisplay = display.innerHTML.split('+');
+                let numbers = splitedDisplay.map(n => parseInt(n, 10));
+                display.innerHTML = sum(numbers[0], numbers[1]);
+            }
+            if (display.innerHTML.includes('-')) {
+                let splitedDisplay = display.innerHTML.split('-');
+                let numbers = splitedDisplay.map(n => parseInt(n, 10));
+                display.innerHTML = take(numbers[0], numbers[1]);
+            }
+            if (display.innerHTML.includes('/')) {
+                let splitedDisplay = display.innerHTML.split('/');
+                let numbers = splitedDisplay.map(n => parseInt(n, 10));
+                display.innerHTML = divide(numbers[0], numbers[1]);
+            }
+            if (display.innerHTML.includes('*')) {
+                let splitedDisplay = display.innerHTML.split('*');
+                let numbers = splitedDisplay.map(n => parseInt(n, 10));
+                display.innerHTML = multiply(numbers[0], numbers[1]);
+
+            // Else, just display the operator
+            } else {
+                display.innerHTML += event.target.innerHTML;
+            }
         } else {
-            console.log('vamooo')
+            
         }
     }
     
@@ -41,3 +64,6 @@ const displayContent = event => {
 
 
 buttons.forEach(button => button.addEventListener('click', displayContent))
+
+console.log(sum(9, 9))
+
