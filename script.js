3d2dc945e46f7d4fa60521c85ade6876f7ecf954
fran <francisco.chiarino@gmail.com>
2020-02-22 02:26:36 +0100

    Getting better;

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 const buttons = document.querySelectorAll('button');
 const display = document.getElementById('display');
-const equals = document.getElementById('equals');
 
 // Functions
 const sum = (a, b) => a + b;
@@ -8,6 +7,35 @@ const take = (a, b) => a - b;
 const divide = (a, b) => a / b;
 const multiply = (a, b) => a * b;
 const clear = () => display.innerHTML = '';
+
+ //Check if there already is an operator and run the corresponding function
+ const runCalculation = () => {
+
+    if (display.innerHTML.includes('+')) {
+        let splitedDisplay = display.innerHTML.split('+');
+        let numbers = splitedDisplay.map(n => parseInt(n, 10));
+        display.innerHTML = sum(numbers[0], numbers[1]);
+    }
+    if (display.innerHTML.includes('-')) {
+        let splitedDisplay = display.innerHTML.split('-');
+        let numbers = splitedDisplay.map(n => parseInt(n, 10));
+        display.innerHTML = take(numbers[0], numbers[1]);
+    }
+    if (display.innerHTML.includes('/')) {
+        let splitedDisplay = display.innerHTML.split('/');
+        let numbers = splitedDisplay.map(n => parseInt(n, 10));
+        display.innerHTML = divide(numbers[0], numbers[1]);
+    }
+    if (display.innerHTML.includes('*')) {
+        let splitedDisplay = display.innerHTML.split('*');
+        let numbers = splitedDisplay.map(n => parseInt(n, 10));
+        display.innerHTML = multiply(numbers[0], numbers[1]);
+
+    // Else, just display the operator
+    } else {
+        display.innerHTML += event.target.innerHTML;
+    }
+}
 
 // Display 
 const displayContent = event => {
@@ -30,40 +58,19 @@ const displayContent = event => {
         
         if (displayLastCharacter >= 0 && displayLastCharacter <= 9) {
 
-            //Check if there already is an operator and run the corresponding function
-            if (display.innerHTML.includes('+')) {
-                let splitedDisplay = display.innerHTML.split('+');
-                let numbers = splitedDisplay.map(n => parseInt(n, 10));
-                display.innerHTML = sum(numbers[0], numbers[1]);
-            }
-            if (display.innerHTML.includes('-')) {
-                let splitedDisplay = display.innerHTML.split('-');
-                let numbers = splitedDisplay.map(n => parseInt(n, 10));
-                display.innerHTML = take(numbers[0], numbers[1]);
-            }
-            if (display.innerHTML.includes('/')) {
-                let splitedDisplay = display.innerHTML.split('/');
-                let numbers = splitedDisplay.map(n => parseInt(n, 10));
-                display.innerHTML = divide(numbers[0], numbers[1]);
-            }
-            if (display.innerHTML.includes('*')) {
-                let splitedDisplay = display.innerHTML.split('*');
-                let numbers = splitedDisplay.map(n => parseInt(n, 10));
-                display.innerHTML = multiply(numbers[0], numbers[1]);
-
-            // Else, just display the operator
-            } else {
-                display.innerHTML += event.target.innerHTML;
-            }
-        } else {
-            
-        }
+            // Function call
+            runCalculation();
+        }   
     }
-    
+
+    // Equal
+    if (event.target.innerHTML === '=') {
+        display.innerHTML = runCalculation();
+    }
+        
 }
 
 
 buttons.forEach(button => button.addEventListener('click', displayContent))
 
-console.log(sum(9, 9))
 
